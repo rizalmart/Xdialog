d9cbd084b74a2b60b16594159485d0f944366d89
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2025-07-05 19:09:13 +0800

    Update interface.c

--- src/interface.c
+++ src/interface.c
@@ -92,20 +92,25 @@ static void parse_rc_file(void)
 	}
 
 	if (dialog_compat) {
+		
 		rc_str = g_strdup_printf(FIXED_FONT_CSS_STRING,
 			(Xdialog.fixed_font_size ? Xdialog.fixed_font_size : DEFAULT_FIXED_FONT_SIZE)
 		);
-		provider = gtk_css_provider_new();
 		
-		if (gtk_css_provider_load_from_data (GTK_STYLE_PROVIDER(provider), rc_str, -1, NULL)) {
+		provider = gtk_css_provider_new();
+		s_provider=GTK_STYLE_PROVIDER(provider)
+		
+		if (gtk_css_provider_load_from_data (s_provider, rc_str, -1, NULL)) {
 			gtk_style_context_add_provider_for_screen(
 				gdk_screen_get_default(),
-				GTK_STYLE_PROVIDER(provider),
+				s_provider,
 				GTK_STYLE_PROVIDER_PRIORITY_USER
 			);
 		}
+		
 		g_free(rc_str);
 		g_object_unref(provider);
+		g_object_unref(s_provider);
 	}
 }
 #endif
